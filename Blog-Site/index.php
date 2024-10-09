3478cc4af8ade3534bdf434524e84f9670455c04
Binayak Niraula <binayak0128@gmail.com>
2024-10-09 21:03:59 +0545

    Blog-Site home page designed

---
+++ Blog-Site/index.php
@@ -0,0 +1,200 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Blog-Site</title>
+    <link rel="stylesheet" href="style-in.css">
+</head>
+
+<body>
+    <!-- navigation bar -->
+    <nav class="nav-bar">
+        <div class="logo">Blog Site</div>
+        <ul>
+            <li><a href="">Trending</a></li>
+            <li><a href="">Latest</a></li>
+            <li><a href="">Blogs</a></li>
+            <li><a href="">Topics</a></li>
+            <li><a href="">About</a></li>
+            <li><a href="">Profile</a></li>
+        </ul>
+    </nav>
+
+    <!-- content body -->
+    <div class="content">
+
+        <!-- content title -->
+        <div class="header">
+            <div class="title">Latest</div>
+            <div class="view-all"><a href="">View all</a></div>
+        </div>
+
+        <!-- highlighted content -->
+        <div class="highlight">
+            <img src="https://picsum.photos/200">
+            <a href="">
+                <h2>ipsum dolor, sit amet consectetur adipisicing elit. Rerum ipsum numquam fuga.</h2>
+                <div class="info"><span id="author">Author</span> | <span id="upload-time">2 minutes ago</span></div>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Perferendis quae quis sed quasi! Eius repellendus nihil deleniti, corrupti eaque voluptates, iste asperiores expedita nisi enim officia recusandae sapiente doloribus suscipit eum quo nostrum impedit vitae autem! Cupiditate illo amet tempore recusandae quibusdam soluta corporis dolore ex vel? Quo eos quisquam delectus autem voluptatum quasi magni, recusandae, esse cumque soluta consequatur.</p>
+            </a>
+        </div>
+
+        <!-- other latest content  -->
+        <div class="more-latest">
+            <div class="more-card card1">
+                <img src="https://picsum.photos/200">
+                <a href="">
+                    <h3>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dolor accusamus necessitatibus unde.
+                    </h3>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint, maiores facilis magnam
+                        consequuntur quas commodi!</p>
+                </a></div>
+            <div class="more-card card2">
+                <img src="https://picsum.photos/200"><a href="">
+                    <h3>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dolor accusamus necessitatibus unde.
+                    </h3>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint, maiores facilis magnam
+                        consequuntur quas commodi!</p>
+                </a></div>
+            <div class="more-card card3">
+                 <img src="https://picsum.photos/200">
+                <a href="">
+                    <h3>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dolor accusamus necessitatibus unde.
+                    </h3>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint, maiores facilis magnam
+                        consequuntur quas commodi!</p>
+                </a></div>
+            <div class="more-card card4">
+                 <img src="https://picsum.photos/200">
+                <a href="">
+                    <h3>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dolor accusamus necessitatibus unde.
+                    </h3>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint, maiores facilis magnam
+                        consequuntur quas commodi!</p>
+                </a></div>
+            <div class="more-card card5">
+                 <img src="https://picsum.photos/200">
+                <a href="">
+                    <h3>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dolor accusamus necessitatibus unde.
+                    </h3>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint, maiores facilis magnam
+                        consequuntur quas commodi!</p>
+                </a></div>
+            <div class="more-card card6">
+                 <img src="https://picsum.photos/200">
+                <a href="">
+                    <h3>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dolor accusamus necessitatibus unde.
+                    </h3>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint, maiores facilis magnam
+                        consequuntur quas commodi!</p>
+                </a></div>
+        </div>
+
+
+        <!-- trending content  -->
+        <div class="header">
+            <div class="title">Trending</div>
+            <div class="view-all"><a href="">View all</a></div>
+        </div>
+        <div class="highlight">
+            <img src="https://picsum.photos/200">
+            <a href="">
+                <h2>ipsum dolor, sit amet consectetur adipisicing elit. Rerum ipsum numquam fuga.</h2>
+                <div class="info"><span id="author">Author</span> | <span id="upload-time">2 minutes ago</span></div>
+                <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quisquam omnis excepturi quidem amet deserunt cumque porro corrupti, error officiis in dignissimos dicta, reprehenderit magni officia? Atque sit id dolores incidunt, accusantium ullam corporis expedita veniam amet quaerat dolorum illum ipsam quibusdam ratione perspiciatis facilis in dicta vitae nulla obcaecati? Molestiae facere sequi omnis quibusdam mollitia inventore delectus, impedit quasi totam!</p>
+            </a>
+        </div>
+
+        <div class="more-latest">
+            <div class="more-card card1">
+                <img src="https://picsum.photos/200">
+                <a href="">
+                    <h3>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dolor accusamus necessitatibus unde.
+                    </h3>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint, maiores facilis magnam
+                        consequuntur quas commodi!</p>
+                </a></div>
+            <div class="more-card card2">
+                <img src="https://picsum.photos/200"><a href="">
+                    <h3>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dolor accusamus necessitatibus unde.
+                    </h3>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint, maiores facilis magnam
+                        consequuntur quas commodi!</p>
+                </a></div>
+            <div class="more-card card3">
+                 <img src="https://picsum.photos/200">
+                <a href="">
+                    <h3>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dolor accusamus necessitatibus unde.
+                    </h3>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint, maiores facilis magnam
+                        consequuntur quas commodi!</p>
+                </a></div>
+            <div class="more-card card4">
+                 <img src="https://picsum.photos/200">
+                <a href="">
+                    <h3>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dolor accusamus necessitatibus unde.
+                    </h3>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint, maiores facilis magnam
+                        consequuntur quas commodi!</p>
+                </a></div>
+            <div class="more-card card5">
+                 <img src="https://picsum.photos/200">
+                <a href="">
+                    <h3>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dolor accusamus necessitatibus unde.
+                    </h3>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint, maiores facilis magnam
+                        consequuntur quas commodi!</p>
+                </a></div>
+            <div class="more-card card6">
+                 <img src="https://picsum.photos/200">
+                <a href="">
+                    <h3>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dolor accusamus necessitatibus unde.
+                    </h3>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint, maiores facilis magnam
+                        consequuntur quas commodi!</p>
+                </a></div>
+        </div>
+
+        <!-- topics  -->
+        <div class="header">
+            <div class="title">Topics</div>
+            <div class="view-all"><a href="">View all</a></div>
+        </div>
+
+        <div class="topics-container">
+            <div class="topics"><a href="">Education</a></div>
+            <div class="topics"><a href="">Health</a></div>
+            <div class="topics"><a href="">Economy</a></div>
+            <div class="topics"><a href="">Politics</a></div>
+            <div class="topics"><a href="">Artificial Intelligence</a></div>
+            <div class="topics"><a href="">Job</a></div>
+            <div class="topics"><a href="">Work from home</a></div>
+            <div class="topics"><a href="">Flood</a></div>
+            <div class="topics"><a href="">Dashain</a></div>
+            <div class="topics"><a href="">Festival</a></div>
+        </div>
+
+        <!-- newsletter -->
+         <div class="newsletter">
+            <div class="header">
+                <div class="title">Subscribe to our newsletter</div>
+            </div>
+            <div class="form">
+                <form>
+                    <input type="email" name="email" placeholder="Enter your email">
+                    <input type="submit" name="submit" value="Subscribe">
+                </form>
+            </div>
+         </div>
+       
+    </div>
+
+    <footer>
+        <div class="logo">Blog Site</div>
+        <span>&copy;Binayak Niraula 2081 Ashwin 16</span>
+    </footer>
+</body>
+
+</html>
